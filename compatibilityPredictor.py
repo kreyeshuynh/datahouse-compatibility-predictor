0e1c08107251af47dfaa39327c3fb9bc1744b217
Kaianu Reyes-Huynh <100328606+kreyeshuynh@users.noreply.github.com>
2023-04-12 00:41:45 -1000

    Initial commit for compatibilityPredictor.py

--- compatibilityPredictor.py
+++ compatibilityPredictor.py
@@ -1,34 +1,55 @@
-import json
+import json #For reading and writing .json files
 
+#Open .json file
 with open('input.json', encoding='utf-8') as f:
     file_contents = json.load(f)
 
+
+#Categorizing necessary members (both team and applicant attributes as well as the applicant names)
 team_attributes = [member['attributes'] for member in file_contents['team']]
 applicant_attributes = [member['attributes'] for member in file_contents['applicants']]
-team_size = len(team_attributes)
+applicant_names = [member['name'] for member in file_contents['applicants']]
 
-min_values = {}
+
+#Determine which attributes within the team has the lowest values and put them into min_attributes[]
+min_value = 10
+min_attributes = []
 for attribute in team_attributes[0]:
-    min_value = min(member[attribute] for member in team_attributes)
-    min_values[attribute] = min_value
+    values = [member[attribute] for member in team_attributes]
+    attribute_min = min(values)
+    #Finding a minimum and putting it into the min_attributes[]
+    if attribute_min < min_value:
+        min_value = attribute_min
+        min_attributes = [attribute]
+    #If there is multiple with the same minimum, add them to min_attributes[]
+    elif attribute_min == min_value:
+        min_attributes.append(attribute)
 
-min_weight_attributes = []
-min_weight_value = min(min_values.values())
-for key, value in min_values.items():
-    if value == min_weight_value:
-        min_weight_attributes.append(key)
 
-compatibility_scores = []
-for applicant in file_contents['applicants']:
-    score = 0
-    for attribute in applicant['attributes']:
-        if attribute in min_weight_attributes:
-            score += 1 - (applicant['attributes'][attribute] / 100)
-        else:
-            score += applicant['attributes'][attribute] / 100
-    compatibility_scores.append({'name': applicant['name'], 'score': score/len(applicant['attributes'])})
+#Determine the number of min_attributes and create equal weights for each attribute
+num_min_weight_attributes = len(min_attributes)
+if num_min_weight_attributes > 1:
+    weight = 1 / num_min_weight_attributes
+else:
+    weight = 1
 
-output = {'scoredApplicants': compatibility_scores}
+
+#Find the values of each min_attributes corresponding to the applicants attribute scores
+#Use those scores and divide them by 10 to create a score with 1 being the best for that attribute
+#If there is a weight we can multiply the total score by the weight to find the best score 
+#given each min_attribute of the team
+output_dict = []
+for i, applicant in enumerate(applicant_attributes):
+    scores = []
+    for attribute in min_attributes:
+        score = applicant[attribute] / 10
+        scores.append(score)
+    total_score = sum(scores) * weight
+    output_dict.append({"name": applicant_names[i], "score": round(total_score,2)})
+
+
+#Output data into format of scoredApplicants
+output_dict = {"scoredApplicants": output_dict}
 
 with open('output.json', 'w') as f:
-    json.dump(output, f, indent=4)
+    json.dump(output_dict, f, indent=4)
