5138ed8b37e2d2bf792b78d1e72d36f3b14849b5
Kaianu Reyes-Huynh <100328606+kreyeshuynh@users.noreply.github.com>
2023-04-11 19:30:56 -1000

    Add files via upload

--- compatibilityPredictor.py
+++ compatibilityPredictor.py
@@ -4,13 +4,31 @@ with open('input.json', encoding='utf-8') as f:
     file_contents = json.load(f)
 
 team_attributes = [member['attributes'] for member in file_contents['team']]
+applicant_attributes = [member['attributes'] for member in file_contents['applicants']]
 team_size = len(team_attributes)
 
-weights = {}
-
+min_values = {}
 for attribute in team_attributes[0]:
-    total_value = sum(member[attribute] for member in team_attributes)
-    average_value = total_value / team_size
-    weights[attribute] = average_value
+    min_value = min(member[attribute] for member in team_attributes)
+    min_values[attribute] = min_value
 
-print(weights)
+min_weight_attributes = []
+min_weight_value = min(min_values.values())
+for key, value in min_values.items():
+    if value == min_weight_value:
+        min_weight_attributes.append(key)
+
+compatibility_scores = []
+for applicant in file_contents['applicants']:
+    score = 0
+    for attribute in applicant['attributes']:
+        if attribute in min_weight_attributes:
+            score += 1 - (applicant['attributes'][attribute] / 100)
+        else:
+            score += applicant['attributes'][attribute] / 100
+    compatibility_scores.append({'name': applicant['name'], 'score': score/len(applicant['attributes'])})
+
+output = {'scoredApplicants': compatibility_scores}
+
+with open('output.json', 'w') as f:
+    json.dump(output, f, indent=4)
